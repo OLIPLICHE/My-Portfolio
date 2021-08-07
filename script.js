bd92a2890c02c7b81643575675b458223b82379d
olipliche <olipliche@gmail.com>
2021-08-07 20:58:48 +0100

    Update script js file

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-const store = {
-  object1: {
+const store = [
+  {
+    id: 1,
     name: 'Tonic',
     img: 'image/Snapshoot_Portfolio.svg',
     companyName: 'CANOPY',
@@ -7,9 +8,11 @@ const store = {
     year: 2015,
     description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing.',
     tags: ['html', 'Ruby on rails', 'css', 'javaScript'],
+    seeLive: 'https://raw.githack.com/OLIPLICHE/My-Portfolio/mobileJs/index.html',
   },
 
-  object2: {
+  {
+    id: 2,
     name: 'Multi-Post Stories',
     img: 'image/Snapshoot_Portfolio(3).svg',
     companyName: 'FACEBOOK',
@@ -17,9 +20,11 @@ const store = {
     year: 2015,
     description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing.',
     tags: ['html', 'Ruby on rails', 'css', 'javaScript'],
+    seeLive: 'https://raw.githack.com/OLIPLICHE/My-Portfolio/mobileJs/index.html',
   },
 
-  object3: {
+  {
+    id: 3,
     name: 'Facebook 360',
     img: 'image/Snapshoot_Portfolio(2).svg',
     companyName: 'FACEBOOK',
@@ -27,9 +32,11 @@ const store = {
     year: 2015,
     description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing.',
     tags: ['html', 'Ruby on rails', 'css', 'javaScript'],
+    seeLive: 'https://raw.githack.com/OLIPLICHE/My-Portfolio/mobileJs/index.html',
   },
 
-  object4: {
+  {
+    id: 4,
     name: 'Uber Navigation',
     img: 'image/SnapshootPortfolio(1).svg',
     companyName: 'Uber',
@@ -37,69 +44,101 @@ const store = {
     year: 2018,
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate. ',
     tags: ['html', 'Ruby on rails', 'css', 'javaScript'],
+    seeLive: 'https://raw.githack.com/OLIPLICHE/My-Portfolio/mobileJs/index.html',
   },
-};
+];
 
-const nameID = document.getElementById('name');
-const img = document.getElementById('img');
-const companyName = document.getElementById('companyName');
-const job = document.getElementById('job');
-const year = document.getElementById('store');
-const description = document.getElementById('description');
-const tags = document.getElementById('tags');
-const button1 = document.querySelector('#btn-1');
-const button2 = document.querySelector('#btn-2');
-const button3 = document.querySelector('#btn-3');
-const button4 = document.querySelector('#btn-4');
-const btnClosePop = document.querySelector('.popup-close-btn');
-const popupOverlay = document.querySelector('.popup-overlay');
-const popup = document.getElementById('popup');
+const boxes = document.querySelector('.boxes');
 
-function switchOnPopUp() {
-  popup.classList.remove('hidden');
-}
-
-function changePopup(order) {
-  tags.innerHTML = '';
-  const object = store[`object${order}`];
-  nameID.innerHTML = object.name;
-  img.src = object.img;
-  companyName.innerHTML = object.companyName;
-  job.innerHTML = object.job;
-  year.innerHTML = object.year;
-  description.innerHTML = object.description;
-  const tagsLength = object.tags.length;
-  for (let i = 0; i < tagsLength; i += 1) {
-    const element = document.createElement('li');
-    element.innerHTML = `${object.tags[i]}`;
-    tags.appendChild(element);
-  }
-  switchOnPopUp();
-}
-
-function switchOfPopUp() {
-  popup.classList.add('hidden');
-}
-
-button1.addEventListener('click', () => {
-  switchOnPopUp();
-  changePopup(1);
+store.forEach((item) => {
+  boxes.innerHTML += `<li class="card-work">
+    <a data-index-number=${item.id} class="portfolio-img" href="#">
+      <img  src=${item.img} alt="tonic" />
+    </a>
+  <div class="right-content">
+    <div class="title">
+      <h2>${item.name}</h2>
+    </div>
+    <div class="info">
+      <span>${item.companyName}</span>
+      <span><img src="Counter.png" alt="counter" /></span>
+      <span>${item.job}</span>
+      <span><img src="Counter.png" alt="counter" /></span>
+      <span>${item.year}</span>
+    </div>
+    <div class="detail">
+      <p>
+        ${item.description}
+      </p>
+    </div>
+    <ul class="languages">
+    ${(function technoUsed() {
+    return item.tags.map((technology) => `<li><a href="#">${technology}</a></li>`).join('');
+  }())}
+  
+    </ul>
+    <div class="see-project">
+      <a class="btn" data-index-number=${item.id} href="">See Project</a>
+    </div>
+  </div>
+</li> `;
 });
 
-button2.addEventListener('click', () => {
-  switchOnPopUp();
-  changePopup(2);
+const cardPopupWindow = document.querySelector('.card-popup-window');
+const seeProjectBtn = document.querySelectorAll('.see-project .btn');
+const seeProjectImg = document.querySelectorAll('.card-work .portfolio-img');
+const closedWind = document.querySelector('.card-popup-window .popup-main-container .main-container .close');
+
+seeProjectImg.forEach((btn) => {
+  btn.addEventListener('click', (e) => {
+    e.preventDefault();
+  });
 });
 
-button3.addEventListener('click', () => {
-  switchOnPopUp();
-  changePopup(3);
+[seeProjectBtn].forEach((item) => {
+  item.forEach((link) => {
+    link.addEventListener('click', (e) => {
+      e.preventDefault();
+      cardPopupWindow.classList.add('open-pop-up');
+      const filteredCard = store.filter((card) => card.id === Number(link.dataset.indexNumber));
+      const title = document.querySelector('.card-popup-window .popup-main-container .main-container .title h2');
+      const info = document.querySelectorAll('.card-popup-window .main-container .info span');
+      const img = document.querySelector('.card-popup-window .main-container .portfolio-img img');
+      const detail = document.querySelector('.card-popup-window .main-container .detail p');
+      const techno = document.querySelector('#lang');
+      const links = document.querySelectorAll('.card-popup-window .main-container .links a');
+
+      title.innerHTML = filteredCard[0].name;
+      img.src = filteredCard[0].img;
+      detail.innerHTML = filteredCard[0].description;
+
+      for (let i = 0; i < Array.from(info).length; i += 1) {
+        if (i === 0) {
+          info[i].innerHTML = filteredCard[0].companyName;
+        } else if (i === 2) {
+          info[i].innerHTML = filteredCard[0].job;
+        } else if (i === 4) {
+          info[i].innerHTML = filteredCard[0].year;
+        }
+      }
+
+      const technologiesChild = [...techno.childNodes];
+      technologiesChild.forEach((child, index) => {
+        techno.removeChild(technologiesChild[index]);
+      });
+      filteredCard[0].tags.forEach((item) => {
+        const techItem = document.createElement('li');
+        const techLink = document.createElement('a');
+        techLink.href = '#';
+        techLink.innerText = item;
+        techItem.appendChild(techLink);
+        techno.appendChild(techItem);
+      });
+      links[0].href = filteredCard[0].seeLive;
+    });
+  });
 });
 
-button4.addEventListener('click', () => {
-  switchOnPopUp();
-  changePopup(4);
+closedWind.addEventListener('click', () => {
+  cardPopupWindow.classList.remove('open-pop-up');
 });
-
-popupOverlay.addEventListener('click', switchOfPopUp);
-btnClosePop.addEventListener('click', switchOfPopUp);
