3936324e2c589e2723cfce460f6fc4dbb99d7889
olipliche <olipliche@gmail.com>
2021-08-07 21:10:46 +0100

    Update validation js file

--- validation.js
+++ validation.js
@@ -1,7 +1,5 @@
 const form = document.getElementById('form-contact');
-const inputName = document.getElementById('name');
 const email = document.getElementById('email');
-const message = document.getElementById('message');
 const msg = document.querySelector('.show-msg');
 
 function validationEmail(e) {
@@ -9,7 +7,6 @@ function validationEmail(e) {
     e.preventDefault();
     msg.style.display = 'block';
     msg.innerHTML = 'The email must be in lowercase';
-  } 
+  }
 }
-
 form.addEventListener('submit', validationEmail);
